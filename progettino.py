d4d48158532e610beaf0af12608544a4582c76a9
net <luca.126>
2024-11-20 10:36:40 +0100

    aggiunto metodo rimuovi elemento

--- progettino.py
+++ progettino.py
@@ -3,18 +3,31 @@ def aggiungiElemento(listaSpesa):
     num = None
     while num == None:
         try:
-            num = int(input("Quanti elementi vuoi aggiungere?"))
+            num = int(input("Quanti elementi vuoi aggiungere? "))
         except:
             print("Inserisci un numero")
     
     for i in range(num):
-        elemento = input("Inserisci elemento da aggiungere alla lista")
+        elemento = input("Inserisci elemento da aggiungere alla lista: ")
         listaSpesa.append(elemento)
         print("Elemento aggiunto")
 
 def visualizzaLista(listaSpesa):
-    print(listaSpesa)
+    for ingrediente in listaSpesa:
+        print(f'{listaSpesa.index(ingrediente)} - {ingrediente}')
+
+def rimuoviElemento(listaSpesa):
+    visualizzaLista(listaSpesa)
+    indice = None
+    while indice == None:
+        try:
+            indice = int(input("Inserisci indice elemento da rimuovere: "))
+        except:
+            print('Inserisci valore numerico')
+    listaSpesa.pop(indice)
+    visualizzaLista(listaSpesa)
 
 listaSpesa = []
 aggiungiElemento(listaSpesa)
 visualizzaLista(listaSpesa)
+rimuoviElemento(listaSpesa)
