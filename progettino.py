1eaeb7b0850ea87f9fcc8d45c0004be62a665179
net <luca.126>
2024-11-20 10:17:33 +0100

    aggiunto metodo aggiungi elemento

---
+++ progettino.py
@@ -0,0 +1,18 @@
+
+def aggiungiElemento(listaSpesa):
+    num = None
+    while num == None:
+        try:
+            num = int(input("Quanti elementi vuoi aggiungere?"))
+        except:
+            print("Inserisci un numero")
+    
+    for i in range(num):
+        elemento = input("Inserisci elemento da aggiungere alla lista")
+        listaSpesa.append(elemento)
+        print("Elemento aggiunto")
+
+
+        
+listaSpesa = []
+aggiungiElemento(listaSpesa)
